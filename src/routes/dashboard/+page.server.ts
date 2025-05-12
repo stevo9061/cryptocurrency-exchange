6655c55b747eca92113962ddfd85aff89e5cc164
Stefan <stefan.bittgen@gmx.net>
2025-05-12 14:46:24 +0200

    added server page to dashboard

---
+++ src/routes/dashboard/+page.server.ts
@@ -0,0 +1,11 @@
+import type { PageServerLoad } from './$types';
+import type { Asset } from '$lib/types';
+import { API_KEY } from '$env/static/private';
+
+export const load: PageServerLoad = async () => {
+  const url = 'https://rest.coincap.io/v3/assets';
+  const res = await fetch(`${url}?apiKey=${API_KEY}`);
+    if (!res.ok) throw  new Error('Failed to fetch cryptos');
+    const { data: cryptos } = (await res.json()) as { data: Asset[] };
+    return { cryptos };
+}
